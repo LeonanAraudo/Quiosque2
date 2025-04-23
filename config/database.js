7aed82ae941e097cab2d7c67cd4da36eb6e7388c
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2025-04-23 16:00:01 -0300

    update bd

--- config/database.js
+++ config/database.js
@@ -1,20 +1,15 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(
-  'quiosque2_postgres', // nome do banco
-  'quiosque2_postgres_user', // usuário
-  'TpIL2mEMR5XUiEGl8G9fVkQERNcNvklg', // senha
-  {
-    host: 'dpg-d04g2lruibrs73b2h010-a.oregon-postgres.render.com', // tente com esse domínio completo
-    dialect: 'postgres',
-    port: 5432,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // necessário para o SSL funcionar no Render
-      },
+const sequelize = new Sequelize('quiosque2_postgres', 'quiosque2_postgres_user', 'TpIL2mEMR5XUiEGl8G9fVkQERNcNvklg', {
+  host: 'dpg-d04g2lruibrs73b2h010-a.oregon-postgres.render.com',  // Host completo do Render
+  dialect: 'postgres',
+  port: 5432,  // Porta padrão para PostgreSQL
+  dialectOptions: {
+    ssl: {
+      require: true,  // Exige uma conexão SSL
+      rejectUnauthorized: false,  // Necessário para evitar erros de conexão com SSL
     },
-  }
-);
+  },
+});
 
 export default sequelize;
